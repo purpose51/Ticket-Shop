757486b2409833dcea246a232fcbc7e66b03e89d
Ayobami Adelakun <purposenigeria@gmail.com>
2020-07-24 04:19:25 +0100

    connect auth to mongodb service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
@@ -21,9 +23,22 @@ app.all('*', async (req, res, next) => {
 })
 app.use(errorHandler)
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to Database')
+    } catch (err) {
+        console.log(err)
+    }
 
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+start()
 
