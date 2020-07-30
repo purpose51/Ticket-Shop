85cfd23eee2010180e3bc94023f3a22a023012de
Ayobami Adelakun <purposenigeria@gmail.com>
2020-07-30 02:13:43 +0100

    create and access secrets with kubernetes access env variable in auth pod

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -39,7 +39,7 @@ router.post('/api/users/signup', [
   const userJwt = jwt.sign({
     id: user.id,
     email: user.email
-  }, 'asdf')
+  }, process.env.JWT_KEY!)
 
   // set it on the session object
   req.session = {
